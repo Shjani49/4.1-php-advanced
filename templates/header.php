c89cd196b8c2d743d4de28df1c6ea6a51fdc985b
Shjani49 <shjani49@gmail.com>
2020-08-12 10:10:06 -0600

    Add navigation; add stylesheet + JavaScript file.

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-    <?php echo $GLOBALS['title']; ?> | TechCareers
+    <?php echo $GLOBALS['title']; ?> | TECHCareers
     </title>
+
+    <!-- Style(s) -->
+    <link rel ="stylesheet" href ="css/main.css">
+
+    <!-- Script(s) -->
+    <script type ="text/JavaScript" src ="js/scripts.js" defer></script>
+    
 </head>
 <body>
